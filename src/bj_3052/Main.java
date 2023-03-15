1d3c7cc6b717365cbb74cac3115d9ca56071488e
Rommmu <tonty1222@naver.com>
2023-03-15 23:27:31 +0900

    BOJ 3052

---
+++ src/bj_3052/Main.java
@@ -0,0 +1,24 @@
+package bj_3052;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[42];
+        int sum = 0;
+        for (int i :array) array[i] = 0;
+
+        // 수 10개 입력 받기
+        for (int i = 0; i < 10; ++i) {
+            int n = scanner.nextInt();
+            array[n % 42] = 1;
+        }
+
+        for (int i : array) {
+            if (i == 1) ++sum;
+        }
+        System.out.println(sum);
+
+    }
+}
