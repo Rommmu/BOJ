5467d6025603cff66a7a93ef882b9db8c442403e
Rommmu <tonty1222@naver.com>
2023-03-17 16:47:15 +0900

    Modified 1546

--- src/bj_1546/Main.java
+++ src/bj_1546/Main.java
@@ -6,5 +6,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int N = scanner.nextInt();
+        double[] array = new double[N];
+        double max = 0;
+
+        for(int i = 0; i < array.length; i++) {
+            int score = scanner.nextInt();
+            array[i] = score;
+            if (array[i] > max) max = array[i];
+        }
+
+        double sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (array[i] / max) * 100;
+            sum += array[i];
+        }
+
+        System.out.println(sum / array.length);
     }
 }
