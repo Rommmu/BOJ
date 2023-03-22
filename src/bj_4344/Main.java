a412e370d3af0316b5475bfc051ba15c84221f2a
Rommmu <tonty1222@naver.com>
2023-03-22 13:07:28 +0900

    BOJ 4344

---
+++ src/bj_4344/Main.java
@@ -0,0 +1,32 @@
+package bj_4344;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int C = scanner.nextInt();
+
+        for (int i = 0; i < C; i++) {
+            int N = scanner.nextInt();
+            double[] array = new double[N];
+            double sum = 0, avg, avgPercent;
+            int avgNum = 0;
+
+            for (int j = 0; j < N; j++) {
+                int score = scanner.nextInt();
+                array[j] = score;
+                sum += score;
+            }
+
+            avg = sum / (double) array.length;
+
+            for (Double d : array) {
+                if (d > avg) ++avgNum;
+            }
+
+            avgPercent = avgNum / (double) N * 100;
+            System.out.println(String.format("%.3f", avgPercent) + "%");
+        }
+    }
+}
