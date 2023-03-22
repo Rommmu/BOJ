06bddbc7d17323fc4bfd5bf9bec18b5a5c5a0b94
Rommmu <tonty1222@naver.com>
2023-03-22 13:07:07 +0900

    BOJ 10812 해결중 ..

---
+++ src/bj_10812/Main.java
@@ -0,0 +1,34 @@
+package bj_10812;
+
+// 푸는 중 ...
+import java.util.Scanner;
+
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int N = scanner.nextInt();
+//        int M = scanner.nextInt();
+//        int[] array = new int[N];
+//
+//        for (int i = 0; i < N; i++) {
+//            array[i] = i + 1;
+//        }
+//
+//        for (int n = 0; n < M; n++) {
+//            // 인덱스
+//            int i = scanner.nextInt() - 1;
+//            int j = scanner.nextInt() - 1;
+//            int k = scanner.nextInt() - 1;
+//
+//            // begin == mid
+//            for (int n1 = k; n1 <= j; n1++, i++) {
+//                int temp = array[i];
+//                array[i] = array[n1];
+//                array[k + 1] = temp;
+//            }
+//
+//            for (int p : array) System.out.print(p);
+//            System.out.println();
+//        }
+//    }
+//}
