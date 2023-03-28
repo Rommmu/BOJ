4eab7d4483102c23c0e0b59de8f84a35302b8de4
Rommmu <tonty1222@naver.com>
2023-03-28 20:49:20 +0900

    BOJ 2501

---
+++ src/bj_2501/Main.java
@@ -0,0 +1,23 @@
+package bj_2501;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        int count = 0;
+        int result = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) count++;
+            if (count == K) {
+                result = i;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
