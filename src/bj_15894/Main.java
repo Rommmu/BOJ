ca34cfaf986cff968581b5fb8004245f175cf766
Rommmu <tonty1222@naver.com>
2023-04-13 21:26:44 +0900

    BOJ 15894

---
+++ src/bj_15894/Main.java
@@ -0,0 +1,13 @@
+package bj_15894;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long N = scanner.nextInt();
+
+        System.out.println(3 * N + N);
+    }
+}
