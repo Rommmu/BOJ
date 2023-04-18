f89f59804a4cb9a8089b2f800b513153204e573d
Rommmu <tonty1222@naver.com>
2023-04-18 16:16:53 +0900

    BOJ 24263

---
+++ src/bj_24263/Main.java
@@ -0,0 +1,14 @@
+package bj_24263;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        // 수행횟수 1 to n 이면 n - 1 + 1
+        System.out.println(N);
+        System.out.println(1);
+    }
+}
