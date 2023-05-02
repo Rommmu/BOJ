78a61c4f0fac2421a3f2528d0c11ce2ae92a6b11
Rommmu <tonty1222@naver.com>
2023-05-02 15:14:23 +0900

    BOJ 11654

---
+++ src/bj_11654/Main.java
@@ -0,0 +1,13 @@
+package bj_11654;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.next();
+        int result = str.charAt(0);
+        System.out.println(result);
+    }
+}
